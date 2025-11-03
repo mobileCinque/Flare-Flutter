b23d4dea3a81df4d5678ea9678007b2a36ebf69f
mobileCinque <108515565+mobileCinque@users.noreply.github.com>
2025-11-03 15:57:33 +0530

    Update asset_flare.dart

--- flare_flutter/lib/provider/asset_flare.dart
+++ flare_flutter/lib/provider/asset_flare.dart
@@ -22,7 +22,8 @@ class AssetFlare extends AssetProvider {
   });
 
   @override
-  int get hashCode => hashValues(bundle, name);
+  int get hashCode => Object.hash(bundle, name);
+
 
   @override
   bool operator ==(dynamic other) {
